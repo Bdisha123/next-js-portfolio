95ce45038ca5426110162521036f94395aa0c757
Bdisha123 <bidishamo14012003@gmail.com>
2024-05-25 12:45:46 +0530

    footer updated

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
                 </div>
 
                 <div className="mb-[20px] text-[15px] text-center">
-                    &copy; WebChain Dev 2023 Inc. All rights reserved
+                    &copy; Bidisha Mondal 2024. All rights reserved
                 </div>
             </div>
         </div>

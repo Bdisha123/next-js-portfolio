70104bcb1711cfe61e0a4ecfa6210cedf8aec6a9
Bdisha123 <bidishamo14012003@gmail.com>
2024-05-25 12:26:58 +0530

    first commit

---
+++ components/main/Projects.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import ProjectCard from "../sub/ProjectCard"
+
+const Projects = () => {
+    return (
+        <div
+            className="flex flex-col items-center justify-center pt-2 pb-20"
+            id="projects"
+        >
+            <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+                My Projects
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-10">
+                <ProjectCard
+                    src="/p1.png"
+                    title="Blog App"
+                    skills="Skills : NextJS, Prisma, Mongodb "
+                    description=""
+
+                />
+                <ProjectCard
+                    src="/p2.png"
+                    title="Real Time Chat Application"
+                    skills="Skill :  ReactJS, Node JS, Express jS, Firebase, SASS"
+                    description=""
+                />
+                <ProjectCard
+                    src="/p3.png"
+                    title="Fiverr Webbsite clone"
+                    skills="Skills: ReactJS, SASS"
+                    description=""
+                />
+                <ProjectCard
+                    src="/p4.png"
+                    title="React.js Portfolio"
+                    skills="Skills: ReactJS, Node JS,Express jS, Nodemailer,SASS"
+                    description=""
+                />
+                <ProjectCard
+                    src="/p5.png"
+                    title="NextJS Portfolio"
+                    skills="Skills: NextJS, Typescript, Framer motion, Three.js "
+                    description=""
+                />
+                <ProjectCard
+                    src="/p6.png"
+                    title="US Election Data Analysis"
+                    skills="Skills: MySQL, ReactJS"
+                    description=""
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
---
+++ constants/index.ts
@@ -0,0 +1,335 @@
+export const Skill_data = [
+    {
+        skill_name: "Html 5",
+        Image: "/html.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Css",
+        Image: "/css.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Java Script",
+        Image: "/js.png",
+        width: 65,
+        height: 65,
+    },
+    {
+        skill_name: "Tailwind Css",
+        Image: "/tailwind.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "React",
+        Image: "/react.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Redux",
+        Image: "/redux.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "React Query",
+        Image: "/reactquery.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Type Script",
+        Image: "/ts.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Next js 13",
+        Image: "/next.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Framer Motion",
+        Image: "/framer.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Stripe Payment",
+        Image: "/stripe.webp",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Node js",
+        Image: "/node-js.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Mongo db",
+        Image: "/mongodb.png",
+        width: 40,
+        height: 40,
+    },
+
+];
+
+export const Socials = [
+    {
+        name: "Linkedin",
+        src: "/linkedin.svg",
+        link:"http://surl.li/txlll",
+    },
+    {
+        name: "Instagram",
+        src: "/instagram.svg",
+        link:"http://surl.li/txlln",
+    },
+    {
+        name: "Github",
+        src: "/github.svg",
+        link:"http://surl.li/txllw",
+    },
+    {
+        name: "Gmail",
+        src: "/gmail.svg",
+        link:"mailto:bidishamo14012003@gmail.com",
+    },
+];
+
+
+export const Frontend_skill = [
+    {
+        skill_name: "Html 5",
+        Image: "/html.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Css",
+        Image: "/css.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Java Script",
+        Image: "/js.png",
+        width: 65,
+        height: 65,
+    },
+    {
+        skill_name: "Tailwind Css",
+        Image: "/tailwind.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Material UI",
+        Image: "/mui.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "React",
+        Image: "/react.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Redux",
+        Image: "/redux.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "React Query",
+        Image: "/reactquery.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Type Script",
+        Image: "/ts.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Next js 13",
+        Image: "/next.png",
+        width: 80,
+        height: 80,
+    },
+];
+
+export const Backend_skill = [
+    {
+        skill_name: "Node js",
+        Image: "/node-js.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Express js",
+        Image: "/express.png",
+        width: 80,
+        height: 80,
+    },
+    {
+        skill_name: "Mongo db",
+        Image: "/mongodb.png",
+        width: 40,
+        height: 40,
+    },
+    {
+        skill_name: "Fire base",
+        Image: "/Firebase.png",
+        width: 55,
+        height: 55,
+    },
+    {
+        skill_name: "Postger SQL",
+        Image: "/postger.png",
+        width: 70,
+        height: 70,
+    },
+    {
+        skill_name: "My SQL",
+        Image: "/mysql.png",
+        width: 70,
+        height: 70,
+    },
+    {
+        skill_name: "Prisma",
+        Image: "/prisma.webp",
+        width: 70,
+        height: 70,
+    },
+    {
+        skill_name: "Graphql",
+        Image: "/graphql.png",
+        width: 80,
+        height: 80,
+    },
+];
+
+export const Full_stack = [
+    {
+        skill_name: "React Native",
+        Image: "/ReactNative .png",
+        width: 70,
+        height: 70,
+    },
+    {
+        skill_name: "Tauri",
+        Image: "/tauri.svg",
+        width: 70,
+        height: 70,
+    },
+    {
+        skill_name: "Docker",
+        Image: "/docker.webp",
+        width: 70,
+        height: 70,
+    },
+
+    {
+        skill_name: "Figma",
+        Image: "/figma.png",
+        width: 50,
+        height: 50,
+    },
+
+];
+
+
+
+export const Other_skill = [
+    {
+        skill_name: "Go",
+        Image: "/go.png",
+        width: 60,
+        height: 60,
+    },
+];
+
+
+
+
+
+export const projects = [
+    /* 1streaming platform */
+    {
+        id: 0,
+        title: "K-Stream",
+        date: "Apr 2024 (developing)",
+        description:
+            "Developed Frontrnd web application that allows users to search for, play, and pause their favorite movies,videos.... Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices.",
+        image:
+            "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+        tags: ["React Js", "MongoDb", "Redux", "javascript", "Tailwand", "css"],
+        category: "web app",
+        github: "",
+        webapp: "",
+        member: [
+            {
+                name: "Karthik",
+                /* add image here */
+                linkedin: "",
+                github: "",
+            },
+
+        ],
+    },
+
+
+
+
+
+
+    {
+        id: 5,
+        title: "Todo Web App",
+        date: "Jun 2024(jan)",
+        description:
+            " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+        image:
+            "https://assets.materialup.com/uploads/16d7673d-0fe6-49b5-b4f4-d249707ef32c/preview.png",
+        tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+        category: "web app",
+        github: "",
+        webapp: "",
+    },
+    /* Quiz app */
+    {
+        id: 7,
+        title: "Quiz App",
+        date: "Dec 2023",
+        description:
+            "A web app made with Frontend . It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
+        image:
+            "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
+        tags: ["Javascript", "React", "Css", "Tailwand"],
+        category: "web app",
+        github: "",
+        webapp: "",
+    },
+    {
+        id: 8,
+        title: "Face Recognition",
+        date: "May 2023",
+        description:
+            "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+        image:
+            "https://d24a06rmrvoemw.cloudfront.net/wp-content/uploads/2021/02/Thumbnail_facial-recognition%402x.png",
+        tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+        category: "machine learning",
+        github: "",
+        webapp: "",
+    },
+];

0eaeb314e233132bb0d7216d4aa11641d6950d99
Bdisha123 <bidishamo14012003@gmail.com>
2024-06-08 09:41:39 +0530

    projects updated

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -15,6 +15,7 @@ const Projects = () => {
                     src="/p1.png"
                     title="Blog App"
                     skills="Skills : NextJS, Prisma, Mongodb "
+                    url = "https://nextjs-blog-app-umber.vercel.app/"
                     description=""
 
                 />
@@ -22,30 +23,35 @@ const Projects = () => {
                     src="/p2.png"
                     title="Real Time Chat Application"
                     skills="Skill :  ReactJS, Node JS, Express jS, Firebase, SASS"
+                    url="https://clinquant-douhua-14bda4.netlify.app/"
                     description=""
                 />
                 <ProjectCard
                     src="/p3.png"
                     title="Fiverr Webbsite clone"
                     skills="Skills: ReactJS, SASS"
+                    url="https://idyllic-panda-ba1909.netlify.app/"
                     description=""
                 />
                 <ProjectCard
                     src="/p4.png"
                     title="React.js Portfolio"
                     skills="Skills: ReactJS, Node JS,Express jS, Nodemailer,SASS"
+                    url="https://portfolio-react-gtkw.onrender.com/"
                     description=""
                 />
                 <ProjectCard
                     src="/p5.png"
                     title="NextJS Portfolio"
                     skills="Skills: NextJS, Typescript, Framer motion, Three.js "
+                    url="https://next-js-portfolio-sable.vercel.app/"
                     description=""
                 />
                 <ProjectCard
                     src="/p6.png"
                     title="US Election Data Analysis"
                     skills="Skills: MySQL, ReactJS"
+                    url="https://github.com/Bdisha123/us-election"
                     description=""
                 />
             </div>
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -8,24 +8,25 @@ interface Props {
     title: string;
     description: string;
     skills: string;
+    url:string;
 }
 
-const ProjectCard = ({ src, title, skills, description }: Props) => {
+const ProjectCard = ({ src, title, skills, description,url }: Props) => {
     return (
-        <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-            <Link href={'https://www.instagram.com/'}>
+        <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] cursor-pointer z-50">
+            <Link href={`${url}`}target="_blank" passHref >
                 <Image
                     src={src}
                     alt={title}
                     width={1000}
                     height={1000}
-                    className="w-full object-contain"
+                    className="w-full object-contain cursor-pointer"
                 />
             </Link>
 
 
             <div className="relative p-4">
-                <h1 className="text-2xl font-semibold text-white">{title}</h1>
+                <h1 className="text-2xl font-semibold text-white cursor-pointer">{title}</h1>
                 <p className="mt-2 text-gray-300">{skills}</p>
             </div>
         </div>
